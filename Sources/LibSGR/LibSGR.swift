a4878bf1e61d749167f3bb857b1f8184bd3745f8
Leah Therese Totland <55258334+lherese@users.noreply.github.com>
2020-03-14 19:51:15 +0100

    Pixel data.

--- Sources/LibSGR/LibSGR.swift
+++ Sources/LibSGR/LibSGR.swift
@@ -1,5 +1,39 @@
 import libsg
 
+func printImage(file: SGFile, index: UInt32) {
+  guard
+    let image = file[image: index]
+  else {
+    return print("Could not load image \(index)")
+  }
+
+  let (height, width) = (image.height - 1, image.width - 1)
+
+  print("Image \(index) is \(image.width) by \(image.height) pixels")
+
+  guard
+    let data = image.data
+  else {
+    return print("Could not load image data")
+  }
+
+  let pixels = data.pixels
+
+  for j in 0...height {
+    var line = ""
+
+    for i in 0...width {
+      if pixels[Int(j * width + i)].alpha > 0 {
+        line += "#"
+      } else {
+        line += " "
+      }
+    }
+
+    print(line)
+  }
+}
+
 func readFile(named name: String) {
 
   guard
@@ -10,12 +44,7 @@ func readFile(named name: String) {
 
   print("File \(name) contains \(file.bitmapCount) bitmaps and \(file.imageCount) images")
 
-  let index: UInt32 = 0
-  guard
-    let image = file[image: 0]
-  else {
-    return print("Could not load image \(index)")
+  for index in 0...(file.imageCount - 1) {
+    printImage(file: file, index: index)
   }
-
-  print("Image \(index) is \(image.width) by \(image.height) pixels")
 }
--- Sources/LibSGR/SGFile.swift
+++ Sources/LibSGR/SGFile.swift
@@ -13,20 +13,49 @@ public class SGFile {
 
   public class Image {
     public class Data {
+      let image: Image
       let data: UnsafeMutablePointer<SgImageData>
 
-      init?(image: Image, file: SGFile) {
+      init?(image: Image) {
         guard
-          let data = sg_load_image_data(image.image, file.name)
+          let data = sg_load_image_data(image.image, image.file.name)
         else {
           return nil
         }
 
+        self.image = image
         self.data = data
+      }
+
+      public var width: UInt16 {
+        data.pointee.width
+      }
+
+      public var height: UInt16 {
+        data.pointee.height
+      }
+
+      public var pixels: Array<Pixel> {
+        Array(UnsafeMutableBufferPointer<UInt32>(start: data.pointee.data, count: Int(width * height)))
+          .map { Pixel($0) }
       }
 
       deinit {
         sg_delete_image_data(data)
+      }
+    }
+
+    public struct Pixel {
+      let alpha: UInt8
+      let red: UInt8
+      let green: UInt8
+      let blue: UInt8
+
+      init(_ data: UInt32) {
+        alpha = UInt8((data & 0xff000000) >> 24)
+        red   = UInt8((data & 0x00ff0000) >> 16)
+        green = UInt8((data & 0x0000ff00) >>  8)
+        blue  = UInt8((data & 0x000000ff) >>  0)
       }
     }
 
@@ -38,12 +67,16 @@ public class SGFile {
       self.image = pointer
     }
 
-    var width: UInt16 {
+    public var width: UInt16 {
       sg_get_image_width(image)
     }
 
-    var height: UInt16 {
+    public var height: UInt16 {
       sg_get_image_height(image)
+    }
+
+    public var data: Data? {
+      Data(image: self)
     }
   }
 
